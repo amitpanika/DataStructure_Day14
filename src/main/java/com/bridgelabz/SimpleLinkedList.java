420fdbd2ca54e9591c973d90aa4442804eb78c1c
amit <amitpanika97@gmail.com>
2022-06-22 22:45:59 +0530

    uc1_createSimple_LinkedList

---
+++ src/main/java/com/bridgelabz/SimpleLinkedList.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class SimpleLinkedList {
+
+    public static void main(String[] args) {
+
+
+        LinkedList<Integer> List = new LinkedList<Integer>();
+        List.add(56);
+        List.add(30);
+        List.add(70);
+        System.out.println(List);
+
+    }
+}
